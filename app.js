57a1a65b1466f297ef457c4a9c65a8466f65d062
denis.voloshyn <denis.voloshyn@techs.com.ua>
2015-12-02 19:22:58 +0200

    Added test view

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require('express');
+var jsonfile = require('jsonfile');
+var util = require('util');
+
 var app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	var file = 'output.json';
+	jsonfile.readFile(file, function(err, obj) {
+		res.render('pages/index', {fixtures: obj.fixtures});		
+	});
 });
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Example app listening at http://%s:%s', host, port);
 });
